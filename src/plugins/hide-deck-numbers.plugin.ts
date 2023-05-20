e31e383da8bd3c4efd8531f54a46075dc635eda5
Kagu-chan <k.boese@kagu-chan.de>
2023-05-21 00:24:18 +0200

    migrate hide deck numbers plugin

---
+++ src/plugins/hide-deck-numbers.plugin.ts
@@ -0,0 +1,19 @@
+import { JPDBPlugin } from '../lib/plugin/jpdb-plugin';
+import { PluginOptions } from '../lib/types';
+
+export class HideDeckNumbersPlugin extends JPDBPlugin {
+  protected _pluginOptions: PluginOptions = {
+    activeAt: ['/learn', '/deck-list'],
+    canBeDisabled: true,
+    name: 'Hide Deck-Numbers',
+    runAgain: true,
+    enabledByDefault: false,
+    enableText: 'Hide deck numbers on learn page and deck list',
+  };
+
+  protected run(): void {
+    return this._dom
+      .find<'div'>("[id|='deck']:not([id*='l']):not([id*='n']) .deck-title")
+      .forEach((e) => (e.innerHTML = e.innerHTML.replace(/\d+\. /, '')));
+  }
+}
--- src/plugins/plugins.ts
+++ src/plugins/plugins.ts
@@ -2,6 +2,7 @@ import { JPDBPlugin } from '../lib/plugin/jpdb-plugin';
 import { AutoSortDecksPlugin } from './auto-sort-decks.plugin';
 import { CSSPlugin } from './css.plugin';
 import { GreetingsPlugin } from './greetings.plugin';
+import { HideDeckNumbersPlugin } from './hide-deck-numbers.plugin';
 import { ScrollInDecksPlugin } from './scroll-in-decks.plugin';
 import { ScrollInSettingsPlugin } from './scroll-in-settings.plugin';
 import { UserCSSPlugin } from './user-css.plugin';
@@ -15,4 +16,5 @@ export const Plugins: JPDBPlugin[] = [
   new ScrollInSettingsPlugin(),
   new ScrollInDecksPlugin(),
   new AutoSortDecksPlugin(),
+  new HideDeckNumbersPlugin(),
 ];
