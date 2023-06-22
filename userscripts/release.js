14f36bebffff0ad25dd98693d9ea55131daae41b
Kagu-chan <k.boese@kagu-chan.de>
2023-06-22 22:02:42 +0200

    Test Release for automated releases - bump to 0.1.2

--- userscripts/release.js
+++ userscripts/release.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JPDB.io UserScript Runner
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Runs scripts for JPDB.io
 // @author       Kagu-chan
 // @license      GLP-3.0
