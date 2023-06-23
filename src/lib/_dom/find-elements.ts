3636c2c6a7ae022b36f93b7018e5976d204a9ae0
Kai Böse <k.boese@global-gruppe.com>
2023-06-23 14:37:01 +0200

    add adjacent and allow create on append

---
+++ src/lib/_dom/find-elements.ts
@@ -0,0 +1,18 @@
+export function findElements(selector: string): HTMLElement[];
+export function findElements<K extends keyof HTMLElementTagNameMap>(
+  selector: string,
+  resultTag?: K,
+): HTMLElementTagNameMap[K][];
+export function findElements(domElement: HTMLElement, selector: string): HTMLElement[];
+export function findElements<K extends keyof HTMLElementTagNameMap>(
+  domElement: HTMLElement,
+  selector: string,
+  resultTag?: K,
+): HTMLElementTagNameMap[K][];
+
+export function findElements(p0: string | HTMLElement, p1?: string, _?: string): HTMLElement[] {
+  const root = typeof p0 === 'string' ? document : p0;
+  const selector = typeof p0 === 'string' ? p0 : p1;
+
+  return Array.from(root.querySelectorAll(selector));
+}
