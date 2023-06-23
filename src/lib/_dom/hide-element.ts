f1d098944748153d1064572e41d5eab3188b7246
Kai Böse <k.boese@global-gruppe.com>
2023-06-23 14:43:28 +0200

    fix typing in hide and show element

--- src/lib/_dom/hide-element.ts
+++ src/lib/_dom/hide-element.ts
@@ -1,7 +1,7 @@
 import { resolveElement } from './resolve-element';
 
 export function hideElement(element: string): void;
-export function hideElement<TParent extends HTMLElement = HTMLElement>(element: TParent): void;
+export function hideElement<TElement extends HTMLElement = HTMLElement>(element: TElement): void;
 
 export function hideElement(element: string | HTMLElement): void {
   resolveElement(element).classList.add('hidden');
--- src/lib/_dom/show-element.ts
+++ src/lib/_dom/show-element.ts
@@ -1,7 +1,7 @@
 import { resolveElement } from './resolve-element';
 
 export function showElement(element: string): void;
-export function showElement<TParent extends HTMLElement = HTMLElement>(element: TParent): void;
+export function showElement<TElement extends HTMLElement = HTMLElement>(element: TElement): void;
 
 export function showElement(element: string | HTMLElement): void {
   resolveElement(element).classList.remove('hidden');
