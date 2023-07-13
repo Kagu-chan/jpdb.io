2e704f05c3cd4347f0ebecb82d28649951bc1683
Kai Böse <k.boese@itvm.de>
2023-07-13 23:40:57 +0200

    add grammar points

---
+++ src/modules/tag-grammar/index.ts
@@ -0,0 +1,15 @@
+import { JP_GRAMMAR } from './jp-grammar';
+
+const TAG_GRAMMAR = 'tag-grammar';
+
+jpdb.settings.registerActivatable({
+  name: TAG_GRAMMAR,
+  displayText: 'Highlight grammar in reviews',
+});
+
+jpdb.runAlwaysWhenActive('/review', TAG_GRAMMAR, () => {
+  // eslint-disable-next-line no-console
+  console.log('hello, im here to tag!');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const foo = JP_GRAMMAR;
+});
