7194bf2c49ecd9e93d7d5ea73f637d1cd4e385cc
Kai Böse <k.boese@itvm.de>
2023-07-15 00:07:14 +0200

    allow making bottom bar sticky as well

--- src/modules/sticky-navbar/index.ts
+++ src/modules/sticky-navbar/index.ts
@@ -1,13 +1,20 @@
 const STICKY_NAVBAR: string = 'sticky-navbar';
+const STICKY_FOOTER: string = 'sticky-footer';
 
 jpdb.settings.registerActivatable({
   name: STICKY_NAVBAR,
-  displayText: 'Stick navigation',
-  description: 'Sticks the navigation to the top of the page, thus making it always visible',
+  displayText: 'Fix header navigation',
+  description: 'Sticks the header navigation to the top of the page, thus making it always visible',
   author: 'JawGBoi',
 });
+jpdb.settings.registerActivatable({
+  name: STICKY_FOOTER,
+  displayText: 'Fix footer navigation',
+  description:
+    'Sticks the footer navigation to the bottom of the page, thus making it always visible',
+});
 
-jpdb.runOnceWhenActive(/.*/, STICKY_NAVBAR, () => {
+jpdb.runOnceWhenActive(/^(?!\/review)/, STICKY_NAVBAR, () => {
   jpdb.css.add({
     key: STICKY_NAVBAR,
     css: `
@@ -18,15 +25,47 @@ jpdb.runOnceWhenActive(/.*/, STICKY_NAVBAR, () => {
   width: 100%;
   z-index: 1000;
   background-color: var(--deeper-background-color);
-  box-shadow: 0 4px 2px 0px var(--big-shadow-color);
+  box-shadow: 0 0 10px 2px var(--big-shadow-color);
+  -webkit-box-shadow: 0 0 10px 2px var(--big-shadow-color);
 }
 
 body {
-  padding-top: 70px;
+  padding-top: 80px;
 }
 
 .notifications {
-  top: calc(70px + 2rem);
+  top: calc(80px + 2rem);
+}
+    `,
+  });
+});
+
+jpdb.runOnceWhenActive(/^(?!\/(review|settings))/, STICKY_FOOTER, () => {
+  jpdb.css.add({
+    key: STICKY_FOOTER,
+    css: `
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
+  background-color: var(--deeper-background-color);
+  box-shadow: 0 0 10px 2px var(--big-shadow-color);
+  -webkit-box-shadow: 0 0 10px 2px var(--big-shadow-color);
+
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1.5rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.75rem;
+  justify-content: flex-end;
+  align-items: center;
+  flex-shrink: 0;
+}
+
+body {
+  padding-bottom: 80px;
 }
     `,
   });
