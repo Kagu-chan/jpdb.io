9615238ca9b82eee3fcbcae3d5431271cae2fef6
Kagu-chan <k.boese@kagu-chan.de>
2023-07-20 23:42:31 +0200

    add controls to show hidden decks

--- src/modules/hide-completed-decks/index.ts
+++ src/modules/hide-completed-decks/index.ts
@@ -6,6 +6,7 @@ class HideCompletedDecks {
   constructor() {
     this.register();
 
+    this.addRootCss();
     this.hideCompletedDecks();
     this.hideThresholdDecks();
   }
@@ -36,32 +37,135 @@ class HideCompletedDecks {
     });
   }
 
+  private addRootCss(): void {
+    jpdb.runOnce('/deck-list', () => {
+      if (
+        !jpdb.settings.getActiveState(this.HIDE_COMPLETED_DECKS) &&
+        !jpdb.settings.getActiveState(this.HIDE_THRESHOLD_DECKS)
+      )
+        return;
+
+      jpdb.css.add(
+        `${this.HIDE_COMPLETED_DECKS}${this.HIDE_THRESHOLD_DECKS}`,
+        __load_css('./src/modules/hide-completed-decks/root.css'),
+      );
+    });
+  }
+
   private hideCompletedDecks(): void {
+    let hiddenDecks: number = 0;
+
+    jpdb.runOnceWhenActive('/deck-list', this.HIDE_COMPLETED_DECKS, () => {
+      this.addCompletedDecksCss();
+    });
+
     jpdb.runAlwaysWhenActive('/deck-list', this.HIDE_COMPLETED_DECKS, () => {
       document.jpdb.withElements<'div'>(
         // eslint-disable-next-line max-len
         `.deck${this.DECK_EXCLUSION} .deck-body > div > div:first-of-type > div:first-of-type > div:last-of-type > div:nth-child(3) > div`,
         (textContainer: HTMLDivElement) => {
           if (this.getRecognition(textContainer) === 100) {
-            document.jpdb.hideElement(textContainer.closest('.deck') as unknown as HTMLElement);
+            hiddenDecks++;
+
+            document.jpdb.closestElement(textContainer, '.deck').classList.add('completed');
           }
         },
       );
+
+      this.displayCompletedCounter(hiddenDecks);
     });
   }
 
   private hideThresholdDecks(): void {
+    let hiddenDecks: number = 0;
+
+    jpdb.runOnceWhenActive('/deck-list', this.HIDE_THRESHOLD_DECKS, () => {
+      this.addThresholdReachedCss();
+    });
+
     jpdb.runAlwaysWhenActive('/deck-list', this.HIDE_THRESHOLD_DECKS, () => {
       document.jpdb.withElements(
         // eslint-disable-next-line max-len
         `.deck${this.DECK_EXCLUSION} .deck-body > div > div:first-of-type > div:nth-child(2) > div:last-of-type > div:nth-child(3) > div`,
         (e: HTMLDivElement) => {
           if (this.getRecognition(e) >= this.getDeckTargetCoverage(e)) {
-            document.jpdb.hideElement(e.closest('.deck') as unknown as HTMLElement);
+            hiddenDecks++;
+
+            document.jpdb.closestElement(e, '.deck').classList.add('threshold-reached');
           }
         },
       );
+
+      this.displayThresholdReachedCounter(hiddenDecks);
     });
+  }
+
+  private displayCompletedCounter(amount: number): void {
+    let isHidden: boolean = true;
+
+    const text = (): string =>
+      `${amount} decks completed` + (!!amount ? ` (${isHidden ? 'show' : 'hide'})` : '');
+
+    const btn = document.jpdb.createElement('span', {
+      class: ['show-hide-control'],
+      innerText: text(),
+      handler: () => {
+        isHidden ? this.removeCompletedDecksCss() : this.addCompletedDecksCss();
+        isHidden = !isHidden;
+
+        btn.innerText = text();
+      },
+    });
+
+    if (!amount) {
+      btn.classList.add('disabled');
+    }
+    document.jpdb.adjacentElement('h4', 'beforeend', btn);
+  }
+
+  private displayThresholdReachedCounter(amount: number): void {
+    let isHidden: boolean = true;
+
+    const text = (): string =>
+      `${amount} decks reached threshold` + (!!amount ? ` (${isHidden ? 'show' : 'hide'})` : '');
+
+    const btn = document.jpdb.createElement('span', {
+      class: ['show-hide-control'],
+      innerText: text(),
+      handler: () => {
+        isHidden ? this.removeThresholdReachedCss() : this.addThresholdReachedCss();
+        isHidden = !isHidden;
+
+        btn.innerText = text();
+      },
+    });
+
+    if (!amount) {
+      btn.classList.add('disabled');
+    }
+    document.jpdb.adjacentElement('h4', 'beforeend', btn);
+  }
+
+  private addCompletedDecksCss(): void {
+    jpdb.css.add(
+      this.HIDE_COMPLETED_DECKS,
+      __load_css('./src/modules/hide-completed-decks/completed.css'),
+    );
+  }
+
+  private removeCompletedDecksCss(): void {
+    jpdb.css.remove(this.HIDE_COMPLETED_DECKS);
+  }
+
+  private addThresholdReachedCss(): void {
+    jpdb.css.add(
+      this.HIDE_THRESHOLD_DECKS,
+      __load_css('./src/modules/hide-completed-decks/threshold-reached.css'),
+    );
+  }
+
+  private removeThresholdReachedCss(): void {
+    jpdb.css.remove(this.HIDE_THRESHOLD_DECKS);
   }
 
   private getTargetCoverage(): string {
