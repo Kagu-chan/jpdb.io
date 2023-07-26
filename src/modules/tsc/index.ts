16329c0e3915ea9d6d68ee8c8580beb3b6e57974
Kagu-chan <k.boese@kagu-chan.de>
2023-07-27 00:27:01 +0200

    reveal TSC target word on tap

--- src/modules/tsc/index.ts
+++ src/modules/tsc/index.ts
@@ -15,6 +15,17 @@
         description:
           // eslint-disable-next-line max-len
           'Also remove the highlight from the target word in the sample sentence, thus turning it into a pure sentence card',
+        hideOrDisable: 'disable',
+        children: [
+          {
+            key: 'rem-highlight-hover',
+            type: 'checkbox',
+            text: 'Tap to reveal',
+            description:
+              // eslint-disable-next-line max-len
+              "Reveals the target word on sentence click or tap - this is useful if the sample sentence contains multiple currently learning words or complex grammar you're not familiar with",
+          },
+        ],
       },
     ],
   });
@@ -24,9 +35,17 @@
       document.jpdb.hideElement('.answer-box .plain');
 
       if (jpdb.settings.persistence.getModuleOption(TSC, 'rem-highlight', false)) {
-        document.jpdb.findElement('.sentence > .highlight').classList.add('rem');
-
         jpdb.css.add(TSC, __load_css('./src/modules/tsc/sentence-card.css'));
+
+        const target = document.jpdb.findElement('.sentence > .highlight');
+        target.classList.add('rem');
+
+        if (jpdb.settings.persistence.getModuleOption(TSC, 'rem-highlight-hover', false)) {
+          const mouseTarget = document.jpdb.findElement('.sentence');
+
+          mouseTarget.addEventListener('click', () => target.classList.remove('rem'));
+          mouseTarget.addEventListener('touch', () => target.classList.remove('rem'));
+        }
       }
     }
   });
