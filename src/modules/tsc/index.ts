3d589aad043be0585cba88647b600e17cd2b9ebf
Kagu-chan <k.boese@kagu-chan.de>
2023-07-30 10:36:40 +0200

    update TSC to allow target hide without word removal

--- src/modules/tsc/index.ts
+++ src/modules/tsc/index.ts
@@ -1,5 +1,6 @@
 ((): void => {
   const TSC = 'TSC';
+  const HIDE_HIGHLIGHT = 'hide-highlight';
 
   jpdb.settings.moduleManager.register({
     name: TSC,
@@ -7,45 +8,45 @@
     displayText: 'Sentence cards',
     description:
       'Remove the target word from reviews, thus turning them into targeted sentence cards',
+  });
+
+  jpdb.settings.moduleManager.register({
+    name: HIDE_HIGHLIGHT,
+    category: 'Reviews',
+    displayText: 'Hide sample sentence target word highlight',
+    description:
+      // eslint-disable-next-line max-len
+      'Remove the target word highlight in sample sentences, avoiding directly recognizing the usage without reading the sentence',
     options: [
       {
-        key: 'rem-highlight',
+        key: 'rem-highlight-hover',
         type: 'checkbox',
-        text: 'Also remove the word highlight',
+        text: 'Tap to reveal',
         description:
           // eslint-disable-next-line max-len
-          'Also remove the highlight from the target word in the sample sentence, thus turning it into a pure sentence card',
-        hideOrDisable: 'disable',
-        children: [
-          {
-            key: 'rem-highlight-hover',
-            type: 'checkbox',
-            text: 'Tap to reveal',
-            description:
-              // eslint-disable-next-line max-len
-              "Reveals the target word on sentence click or tap - this is useful if the sample sentence contains multiple currently learning words or complex grammar you're not familiar with",
-          },
-        ],
+          "Reveals the target word on sentence click or tap - this is useful if the sample sentence contains multiple currently learning words or complex grammar you're not familiar with",
       },
     ],
   });
 
-  jpdb.runOnceWhenActive('/review', TSC, () => {
-    if (document.jpdb.findElement('.kind')?.innerText === 'Vocabulary') {
+  jpdb.runOnceWhenEitherIsActive('/review', [TSC, HIDE_HIGHLIGHT], () => {
+    if (document.jpdb.findElement('.kind')?.innerText !== 'Vocabulary') return;
+
+    if (jpdb.settings.moduleManager.getActiveState(TSC)) {
       document.jpdb.hideElement('.answer-box .plain');
+    }
 
-      if (jpdb.settings.persistence.getModuleOption(TSC, 'rem-highlight', false)) {
-        jpdb.css.add(TSC, __load_css('./src/modules/tsc/sentence-card.css'));
+    if (jpdb.settings.moduleManager.getActiveState(HIDE_HIGHLIGHT)) {
+      jpdb.css.add(TSC, __load_css('./src/modules/tsc/sentence-card.css'));
 
-        const target = document.jpdb.findElement('.sentence > .highlight');
-        target.classList.add('rem');
+      const target = document.jpdb.findElement('.sentence > .highlight');
+      target.classList.add('rem');
 
-        if (jpdb.settings.persistence.getModuleOption(TSC, 'rem-highlight-hover', false)) {
-          const mouseTarget = document.jpdb.findElement('.sentence');
+      if (jpdb.settings.persistence.getModuleOption(TSC, 'rem-highlight-hover', false)) {
+        const mouseTarget = document.jpdb.findElement('.sentence');
 
-          mouseTarget.addEventListener('click', () => target.classList.remove('rem'));
-          mouseTarget.addEventListener('touch', () => target.classList.remove('rem'));
-        }
+        mouseTarget.addEventListener('click', () => target.classList.remove('rem'));
+        mouseTarget.addEventListener('touch', () => target.classList.remove('rem'));
       }
     }
   });
