988fe2b5a6ed6e378c3e2b2e71c261a4ed7a83c3
Kagu-chan <k.boese@kagu-chan.de>
2023-10-20 23:05:38 +0200

    include names in auto fail configuration

--- src/modules/auto-fail-new-cards/index.ts
+++ src/modules/auto-fail-new-cards/index.ts
@@ -3,10 +3,20 @@ enum NEVER_FORGET {
   REMOVE = 'REMOVE',
 }
 
-enum FAIL_AT {
+enum FAIL_AT_WON {
   ALL = 'ALL',
   VOCAB = 'VOCAB',
   KANJI = 'KANJI',
+}
+
+enum FAIL_AT_WN {
+  ALL = 'ALL',
+  VOCAB = 'VOCAB',
+  KANJI = 'KANJI',
+  NAME = 'NAME',
+  VOCAB_KANJI = 'VOCAB_KANJI',
+  VOCAB_NAME = 'VOCAB_NAME',
+  KANJI_NAME = 'KANJI_NAME',
 }
 
 /**
@@ -15,10 +25,19 @@ enum FAIL_AT {
 class AutoFailNewCards {
   private AUTO_FAIL_ENABLE: string = 'auto-fail-new';
   private AUTO_FAIL_MODE: string = 'auto-fail-mode';
-  private AUTO_FAIL_LABELS: Record<FAIL_AT, string> = {
-    [FAIL_AT.ALL]: 'Always',
-    [FAIL_AT.VOCAB]: 'Only Vocab cards',
-    [FAIL_AT.KANJI]: 'Only Kanji cards',
+  private AUTO_FAIL_LABELS_WON: Record<FAIL_AT_WON, string> = {
+    [FAIL_AT_WN.ALL]: 'Always',
+    [FAIL_AT_WN.VOCAB]: 'On Vocab cards',
+    [FAIL_AT_WN.KANJI]: 'On Kanji cards',
+  };
+  private AUTO_FAIL_LABELS_WN: Record<FAIL_AT_WN, string> = {
+    [FAIL_AT_WN.ALL]: 'Always',
+    [FAIL_AT_WN.VOCAB]: 'On Vocab cards',
+    [FAIL_AT_WN.KANJI]: 'On Kanji cards',
+    [FAIL_AT_WN.NAME]: 'On Name cards',
+    [FAIL_AT_WN.VOCAB_KANJI]: 'On Vocab and Kanji cards',
+    [FAIL_AT_WN.VOCAB_NAME]: 'On Vocab and Name cards',
+    [FAIL_AT_WN.KANJI_NAME]: 'On Kanji and Name cards',
   };
 
   private NEVER_FORGET_ENABLE: string = 'move-never-forget';
@@ -40,6 +59,27 @@ class AutoFailNewCards {
         jpdb.settings.hasPatreonPerks() &&
         jpdb.settings.getJpdbRadioSetting('auto-reveal-new-cards')
       ) {
+        const namesEnabled = !jpdb.settings.getJpdbRadioSetting('do-not-learn-non-katakana-names');
+
+        if (
+          [
+            FAIL_AT_WN.NAME,
+            FAIL_AT_WN.VOCAB_KANJI,
+            FAIL_AT_WN.VOCAB_NAME,
+            FAIL_AT_WN.KANJI_NAME,
+          ].includes(
+            jpdb.settings.persistence.getModuleOption(this.AUTO_FAIL_ENABLE, this.AUTO_FAIL_MODE),
+          ) &&
+          !namesEnabled
+        ) {
+          jpdb.settings.moduleManager.disableModule(this.AUTO_FAIL_ENABLE);
+          jpdb.settings.persistence.setModuleOption(
+            this.AUTO_FAIL_ENABLE,
+            this.AUTO_FAIL_MODE,
+            FAIL_AT_WON.ALL,
+          );
+        }
+
         jpdb.settings.moduleManager.register({
           name: this.AUTO_FAIL_ENABLE,
           category: 'Reviews',
@@ -49,9 +89,9 @@ class AutoFailNewCards {
             {
               key: this.AUTO_FAIL_MODE,
               type: 'radio',
-              options: FAIL_AT,
-              labels: this.AUTO_FAIL_LABELS,
-              default: FAIL_AT.ALL,
+              options: namesEnabled ? FAIL_AT_WN : FAIL_AT_WON,
+              labels: namesEnabled ? this.AUTO_FAIL_LABELS_WN : this.AUTO_FAIL_LABELS_WON,
+              default: FAIL_AT_WN.ALL,
             },
           ],
         });
@@ -79,7 +119,7 @@ class AutoFailNewCards {
 
   private addListeners(): void {
     jpdb.runOnceWhenActive('/review', this.AUTO_FAIL_ENABLE, () => {
-      const mode = jpdb.settings.persistence.getModuleOption<FAIL_AT>(
+      const mode = jpdb.settings.persistence.getModuleOption<FAIL_AT_WN>(
         this.AUTO_FAIL_ENABLE,
         this.AUTO_FAIL_MODE,
       );
@@ -87,16 +127,50 @@ class AutoFailNewCards {
       const fail = document.jpdb.findElement('input[value="✘ Fail"]');
       const nothing = document.jpdb.findElement('input[value="✘ Nothing"]');
       const isVocab = !!document.jpdb.findElement('.result.vocabulary');
+      const isName =
+        isVocab &&
+        document.jpdb
+          .findElement('.result.vocabulary .part-of-speech > div')
+          ?.innerText.includes('Name');
 
       if (showAnswer || fail || nothing) {
         return;
       }
 
-      if (
-        mode === FAIL_AT.ALL ||
-        (isVocab && mode === FAIL_AT.VOCAB) ||
-        (!isVocab && mode === FAIL_AT.KANJI)
-      ) {
+      let modifyButtons: boolean = false;
+
+      switch (mode) {
+        case FAIL_AT_WN.ALL:
+          modifyButtons = true;
+
+          break;
+        case FAIL_AT_WN.VOCAB:
+          modifyButtons = isVocab && !isName;
+
+          break;
+        case FAIL_AT_WN.KANJI:
+          modifyButtons = !isVocab;
+
+          break;
+        case FAIL_AT_WN.NAME:
+          modifyButtons = isName;
+
+          break;
+        case FAIL_AT_WN.VOCAB_KANJI:
+          modifyButtons = !isName;
+
+          break;
+        case FAIL_AT_WN.VOCAB_NAME:
+          modifyButtons = isVocab;
+
+          break;
+        case FAIL_AT_WN.KANJI_NAME:
+          modifyButtons = !isVocab || isName;
+
+          break;
+      }
+
+      if (modifyButtons) {
         document.jpdb.withElement('#grade-p', (e) => e.closest('.row.row-1')?.remove());
         document.jpdb.withElement<'input'>('#grade-f', (e: HTMLInputElement) => {
           e.classList.remove('v1');
