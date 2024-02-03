5552ba8b5d96788a9548f2275094510537cf65b1
Kagu-chan <k.boese@kagu-chan.de>
2024-02-03 14:30:40 +0100

    add allow changing the desired viewport width

--- src/modules/widen-viewport/index.ts
+++ src/modules/widen-viewport/index.ts
@@ -1,5 +1,6 @@
 class WidenViewport {
   private WIDEN_VIEWPORT = 'widen-viewport';
+  private DEFAULT_FIX_WIDTH: string = '73rem';
 
   constructor() {
     jpdb.onDesktop(() => {
@@ -16,18 +17,46 @@ class WidenViewport {
       displayText: 'Widen viewport',
       description: 'Widens the viewport of the main frame, making the page wider on bigger screens',
       author: 'JawGBoi',
+      options: [
+        {
+          type: 'text',
+          key: 'fixWidth',
+          text: 'Which width to fix the main frame to',
+          default: this.DEFAULT_FIX_WIDTH,
+        },
+      ],
     });
   }
 
   private addEventListeners(): void {
     jpdb.runOnceOnEnable(/.*/, this.WIDEN_VIEWPORT, () => {
-      jpdb.css.add({
-        key: this.WIDEN_VIEWPORT,
-        css: __load_css('./src/modules/widen-viewport/viewport.css'),
+      this.addCss();
+
+      jpdb.on('update-widen-viewport-fixWidth', () => {
+        jpdb.css.remove(this.WIDEN_VIEWPORT);
+        this.addCss();
       });
     });
 
-    jpdb.runOnceOnDisable(/.*/, this.WIDEN_VIEWPORT, () => jpdb.css.remove(this.WIDEN_VIEWPORT));
+    jpdb.runOnceOnDisable(/.*/, this.WIDEN_VIEWPORT, () => {
+      jpdb.css.remove(this.WIDEN_VIEWPORT);
+      jpdb.clearEvents('update-widen-viewport-fixWidth');
+    });
+  }
+
+  private addCss(): void {
+    const value = jpdb.settings.persistence.getModuleOption<string>(
+      this.WIDEN_VIEWPORT,
+      'fixWidth',
+    );
+    const css = `body, .container.bugfix, .review-reveal, .review-hidden, .text-content {
+      max-width: ${value} !important;
+    }`;
+
+    jpdb.css.add({
+      key: this.WIDEN_VIEWPORT,
+      css,
+    });
   }
 }
 
